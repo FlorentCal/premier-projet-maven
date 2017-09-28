da0dfdf961bf7057e61da0d0efc54f045c2ed3cc
Florent Callaou <florent.callaou22@gmail.com>
2017-09-28 15:21:34 +0200

    Fin tp3 maven

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -10,7 +12,13 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String asciiArt = FigletFont.convertOneLine("Ma super banière");
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiArt = FigletFont.convertOneLine(titre);
         System.out.println(asciiArt);
+        
+        // affichage de l'environnement
+        String environnement = ResourceBundle.getBundle("application").getString("environnement");
+        System.out.println("Environnement : " + environnement);
+
     }
 }
